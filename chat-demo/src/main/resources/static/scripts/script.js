32c4b48ba368962c3f9fea81b580e0c900dc64be
mike <mvteplitsky@yahoo.com>
2024-01-28 18:50:21 -0500

    save demo

---
+++ chat-demo/src/main/resources/static/scripts/script.js
@@ -0,0 +1,79 @@
+   function loadContent(url) {
+            fetch(url)
+                .then(response => response.text())
+                .then(html => {
+                    document.getElementById("mainArea").innerHTML = html;
+                    const fileInput = document.getElementById('fileInput');
+                        if (fileInput) { // Ensure the element exists
+                            fileInput.addEventListener('change', function() {
+                            const fileName = this.files[0] ? this.files[0].name : "";
+                            document.getElementById('responseMessage').innerHTML = "Selected: "+fileName;
+                        });
+                    }
+                });
+        }
+
+        window.onload = function() {
+            loadContent('/search');
+        }
+
+    function performSearch() {
+//    console.log("performSearch function called");
+        var query = document.getElementById("query").value;
+        fetch("/performSearch?query=" + encodeURIComponent(query))
+            .then(response => response.json())
+            .then(data => {
+                // Displaying results as a simple list; modify as needed
+                var resultsHTML = JSON.stringify(data);
+                document.getElementById("searchResults").innerHTML = resultsHTML.replace(/ /g, "&nbsp;").replace(/\n/g, "<br>").replaceAll(",", ",<br>");
+                //hljs.highlightBlock(document.getElementById('searchResults'));
+            });
+    }
+
+    function performDirScan() {
+//    console.log("performDirScan function called");
+        var path = document.getElementById("path").value;
+        fetch("/performDirScan?path=" + encodeURIComponent(path))
+            .then(response => response.json())
+            .then(data => {
+                // Displaying results as a simple list; modify as needed
+                var resultsHTML = "<ul>";
+                data.forEach(item => {
+                    resultsHTML += "<li>" + item + "</li>";
+                });
+                resultsHTML += "</ul>";
+                document.getElementById("responseMessage").innerHTML = resultsHTML;
+            });
+    }
+
+    function sendFile() {
+        let fileInput = document.getElementById("fileInput");
+        let checkbox = document.getElementById("replaceCheckbox");
+
+        let file = fileInput.files[0];
+        if (!file) {
+            alert("Please select a file or directory before submitting.");
+            return;
+        }
+
+        // Use FormData to send the file
+        let formData = new FormData();
+        formData.append("file", file);
+        formData.append("replace", checkbox.checked);//document.getElementById("replaceCheckbox").checked
+
+        fetch("/processFile", {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("responseMessage").innerText = data.message;
+            fileInput.value='';
+        })
+        .catch(error => {
+            document.getElementById("responseMessage").innerText = error;
+            console.error("Error:", error);
+        });
+    }
+
+ /*   END   */
